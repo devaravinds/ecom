5e64ffe3aa7f0e57ebc7dbbb96421b15a79e8d93
mahimaprakash <mahimaprakash05@gmail.com>
2024-04-18 23:20:59 +0530

    added validation for signup api

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,30 +1,28 @@
 const User = require("../models/user-model");
 const roles = require("../enum/roles");
 
-const signup = async(req, res) => {
+const signup = async (req, res) => {
+  const newUser = new User({
+    email: req?.body?.email,
+    name: req?.body?.name,
+    password: req?.body?.password,
+    role: roles.USER,
+  });
 
-    const newUser = new User({
-        email : req?.body?.email,
-        name : req?.body?.name,
-        password : req?.body?.password,
-        role: roles.USER
-    })
-
-    try{
-        await newUser.save();
-        return res.status(201).json({
-            message: "Account successfully created",
-            success: true,
-        }); 
-    }
-    catch(err){
-        console.log(err.message)
-        return res.status(500).json({
-           message:err.message
-        });
-    }
-}
+  try {
+    await newUser.save();
+    return res.status(201).json({
+      message: "Account successfully created",
+      success: true,
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 module.exports = {
-    signup
-}
+  signup,
+};
