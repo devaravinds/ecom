78087373015a098a2ae93cc74a3ea3f941058134
mahimaprakash <mahimaprakash05@gmail.com>
2024-05-25 23:53:44 +0530

    Added changePassword API

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,13 +1,37 @@
 const router = require("express").Router();
-const { signup, login, createAdmin } = require("../controller/user-controller");
-const { ensureAuthenticated, ensureAuthorized } = require("../middleware/user-middleware");
-const { signUpValidation } = require("../middleware/validations/user-validator");
+const {
+  signup,
+  login,
+  createAdmin,
+  changePassword,
+} = require("../controller/user-controller");
+const {
+  ensureAuthenticated,
+  ensureAuthorized,
+} = require("../middleware/user-middleware");
+const {
+  signUpValidation,
+  loginValidation,
+  changePasswordValidation,
+} = require("../middleware/validations/user-validator");
 const roles = require("../enum/roles");
 
 router.post("/signup", signUpValidation, signup);
 
-router.post("/login", login);
+router.post("/login", loginValidation, login);
 
-router.post("/createAdmin", signUpValidation, ensureAuthenticated, ensureAuthorized([roles.SUPER_ADMIN]), createAdmin)
+router.post(
+  "/createAdmin",
+  signUpValidation,
+  ensureAuthenticated,
+  ensureAuthorized([roles.SUPER_ADMIN]),
+  createAdmin
+);
 
+router.put(
+  "/changePassword",
+  changePasswordValidation,
+  ensureAuthenticated,
+  changePassword
+);
 module.exports = router;
